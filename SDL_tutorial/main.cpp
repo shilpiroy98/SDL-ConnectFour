ec95b1870fd5d15d4df38249e3f2378cf1eb5efc
Shilpi Roy <shroy@zynga.com>
2020-11-12 22:16:39 +0530

    Initial Commit

---
+++ SDL_tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_tutorial
+//
+//  Created by Shilpi Roy on 12/11/20.
+//  Copyright © 2020 Shilpi Roy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
